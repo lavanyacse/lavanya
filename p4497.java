3eb2113d945234151a3a355c4db9df66aba5f2f4
lavanyacse <lavanyasubramanian3@gmail.com>
2016-09-06 10:30:54 +0530

    Update p4497.java

--- p4497.java
+++ p4497.java
@@ -1,48 +1,89 @@
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
 
-public class Pro47 {
-	static String kPad[]={"","","ABC","DEF","GHI","JKL","MNO","PRS","TUV","WXY"};
-	public static String check(String str){
-		StringBuffer sb=new StringBuffer("");
-		for(int i=0;i<str.length();i++){
-			char ch=str.charAt(i);
-			if(ch!='-'){
-				if(Character.isDigit(ch))
-					sb.append(ch);
-				else
-					for(int j=2;j<kPad.length;j++)
-						if(kPad[j].contains(ch+"")){
-							sb.append(j);
-							break;
-						}
-			}
-			if(sb.length()==3)
-				sb.append('-');
-		}
-		return sb.toString();
+public class TellePhone {
+	HashMap<Character,Integer> hash=new HashMap<>();
+	TreeMap<String,Integer> hash2=new TreeMap<>();
+	public TellePhone(){
+		hash.put('A',2);
+		hash.put('B',2);
+		hash.put('C',2);
+		hash.put('D',3);
+		hash.put('E',3);
+		hash.put('F',3);
+		hash.put('G',4);
+		hash.put('H',4);
+		hash.put('I',4);
+		hash.put('J',5);
+		hash.put('K',5);
+		hash.put('L',5);
+		hash.put('M',6);
+		hash.put('N',6);
+		hash.put('O',6);
+		hash.put('P',7);
+		hash.put('R',7);
+		hash.put('S',7);
+		hash.put('T',8);
+		hash.put('U',8);
+		hash.put('V',8);
+		hash.put('W',9);
+		hash.put('X',9);
+		hash.put('Y',9);
+		
 	}
+        public void TellephoneEx(String[] str){
+        	
+        	for(int i=0;i<str.length;i++){
+        		String str2="";
+        		for(int j=0;j<str[i].length();j++){
+        			
+        			char c=str[i].charAt(j);
+        			if(str2.length()==3)
+        				str2+='-';
+        			if(Character.isAlphabetic(c)){
+        				int f=hash.get((Character)c);
+        				str2+=f;
+        			}
+        			else if(Character.isDigit(c))
+        				str2+=c;
+        			
+        		}
+        		System.out.println(str2);
+        		if(!hash2.containsKey(str2))
+        		    hash2.put(str2, 1);
+        		else if(hash2.containsKey(str2))
+        			hash2.put(str2,hash2.get(str2)+1);
+        	}
+        	Set set=hash2.keySet();
+        	Iterator it=set.iterator();
+        	while(it.hasNext()){
+        		String key=(String) it.next();
+        		if(hash2.get(key)>1)
+        			System.out.println(key+"  "+hash2.get(key));
+        		
+        	}
+        	
+        }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner s=new Scanner(System.in);
-		int N=s.nextInt();
-		TreeMap<String,Integer> tm=new TreeMap<>();
-		String str;
-		for(int i=0;i<N;i++){
-			str=check(s.next());
-			if(!tm.containsKey(str))
-				tm.put(str, 0);
-			tm.put(str,tm.get(str)+1);
+		Scanner scr=new Scanner(System.in);
+		System.out.println("Enter the Number of numbers");
+		int n=scr.nextInt();
+
+		String[] str = new String[n];
+		for(int i=0;i<n;i++){
+		
+			str[i]=scr.next();
+			
 		}
-		Set<String> st=tm.keySet();
-		Iterator<String> ia=st.iterator();
-		while(ia.hasNext()){
-			str=ia.next();
-			int n=tm.get(str);
-			if(n>=2)
-				System.out.println(str+" "+n);
-		}
+		
+		TellePhone tele=new TellePhone();
+		System.out.println(str[0].length());
+		tele.TellephoneEx(str);
+
 	}
+
 }
